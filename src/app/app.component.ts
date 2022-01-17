3525f59cb58ccd4a1f3e8115c180d2b52590e9e7
Marcus Müller <93290039+MuellerMarcus@users.noreply.github.com>
2022-01-17 09:17:55 +0000

    More Images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'marcus-mueller';
+export class AppComponent implements OnInit {
+ images = ['cathe.jpg', 'modern.jpg', 'mountain.jpg'];
+ currentImage = 0;
+showImage=true;
 
+ ngOnInit() {
+  this.updateImage();
+ }
 
+  updateImage(){
   
+   setInterval(() =>{
+     this.currentImage++;
+     this.currentImage = this.currentImage % this.images.length;
+     this.showImage=false;
+     setTimeout(()=> {
+       this.showImage=true;
+     },10);
+   }, 5000);
+    
+  }
+
+
 }
